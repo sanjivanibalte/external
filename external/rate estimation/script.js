d0e557c47b5e9f7bf5d4add578a217c40f7b25e9
sanjivanibalte <113239870+sanjivanibalte@users.noreply.github.com>
2023-08-01 00:16:16 +0530

    Delete external directory

--- external/rate estimation/script.js
+++
@@ -1,7 +0,0 @@
-function calculate() {
-    const hours = parseFloat(document.getElementById('hours').value);
-    const rate = parseFloat(document.getElementById('rate').value);
-    const result = hours * rate;
-    document.getElementById('result').textContent = `Estimated Amount: $${result.toFixed(2)}`;
-  }
-  
