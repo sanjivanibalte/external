06f6c4ab61c4d7f09269499e7414ff4a4e080282
sanjivanibalte <113239870+sanjivanibalte@users.noreply.github.com>
2023-08-01 00:15:02 +0530

    Delete external/exp-2 directory

--- external/exp-2/main.js
+++
@@ -1,8 +0,0 @@
-window.onload = () => {
-    const button = document.getElementById('menu-button');
-    const menu = document.getElementById('menu');
-
-    button.addEventListener('click', () => {
-        menu.classList.toggle('hidden');
-    });
-};
